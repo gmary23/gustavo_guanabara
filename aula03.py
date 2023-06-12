09c69217bb855cf4de68448109c51ee2ae462035
Geila <geila.estudos@gmail.com>
2023-06-12 16:09:30 -0300

    soma entre dois números

---
+++ aula03.py
@@ -0,0 +1,7 @@
+"Crie um programa que leia dois números e mostre a soma entre eles."
+
+n1 = int(input('Digite um valor: '))
+n2 = int(input('Digite outro valor: '))
+s = n1 + n2
+
+print(f'A soma dos números é {s}')
